0d7f24a39716fdd14e4f027f0c35574ec31277e7
kyj1212 <yongjun3002@naver.com>
2022-09-17 14:07:40 +0900

    hooks project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
+function App(){
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([])
+  const [text, setText] = useState(1)
+  const [cost, setCost] = useState(1)
+
+
+  function handleselect(event){
+    setCost(event.target.value)
+    setText(1)
+  }
+
+  function onChange(event){
+    setText(event.target.value)
+  }
+
+  
+  useEffect (()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(resonponse=>resonponse.json())
+    .then((json) =>{
+      setCoins(json)
+      setLoading(false)
+    })
+  }, [])
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The coins</h1>
+      {loading ? <strong>loading ...</strong> : null}
+      <p>
+        <select onChange={handleselect}>
+          {coins.map(coin=> <option value={coin.quotes.USD.price}> {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}</option>)}
+        </select>
+      </p>
+      <p>
+        <input onChange={onChange} value={text} type='text' placeholder='how convert...'></input>       
+      </p>
+      <h2>you can get {text/cost}</h2>
     </div>
-  );
+  )
 }
 
 export default App;
